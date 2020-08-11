19432855443773e76bbfbeb3d26c65067a34b6cc
Xycart <sssxyc@163.com>
2020-08-11 12:30:05 +0800

    update test

--- test/padssch_rename_refs.py
+++ test/padssch_rename_refs.py
@@ -4,6 +4,7 @@ demo
 """
 import os
 import sys
+import re
 import configparser
 import traceback
 
@@ -20,15 +21,67 @@ App=_mputils_.PADSSCHApplication()
 
 App.Visible=True
 App.StatusBarText="Hello from python: " + __author__ + "@" + str(datetime.datetime.now())
-print(App.Version)
+print("Version", App.Version)
 
-schFile="demo.sch"
+schFile="E:/PADS Projects/Samples/DEMO.SCH"
 if not os.path.exists(schFile):
     App.Quit()
     raise Exception("Invalid schFile!")
 
-Document=App.OpenDocument(schFile)
+Document = IPowerLogicDoc(App.OpenDocument(schFile))
 if Document is None:
     App.Quit()
     raise Exception("Invalid document!")
 
+Sheets = Document.Sheets
+print("SheetsCount", Sheets.Count)
+
+reRefPrefix = re.compile(u'[a-zA-Z]*')
+refSuffix = []
+for idx, Sheet in enumerate(Sheets):
+    sheet = IPowerLogicSheet(Sheet)
+    #print("Sheet%d" % (1 + idx), "ComponentsCount", sheet.Components.Count)
+    refsOld = []
+    refSuffixSheet = {}
+    for Comp in sheet.Components:
+        comp = IPowerLogicComp(Comp)
+        refsOld.append(comp.Name)
+        refPrefix = reRefPrefix.findall(comp.Name)[0]
+        if not refPrefix in refSuffixSheet:
+            refSuffixSheet[refPrefix] = (1 + idx) * 100   
+        else:
+            #refSuffixSheet[refPrefix] = (1 + refSuffixSheet[refPrefix])
+            pass
+    refSuffix.append(refSuffixSheet)
+    print("RefsDesOld", refsOld)
+
+for idx, Sheet in enumerate(Sheets):
+    sheet = IPowerLogicSheet(Sheet)
+    refsTmp = []
+    refSuffixSheet = refSuffix[idx]
+    for Comp in sheet.Components:
+        comp = IPowerLogicComp(Comp)
+        refPrefix = reRefPrefix.findall(comp.Name)[0]
+        if not refPrefix in refSuffixSheet:
+            refSuffixSheet[refPrefix] = (1 + idx) * 100   
+        else:
+            refSuffixSheet[refPrefix] = (1 + refSuffixSheet[refPrefix])
+            pass
+        comp.Name = "X%s%d" % (refPrefix, refSuffixSheet[refPrefix])
+        refsTmp.append(comp.Name)
+    refSuffix.append(refSuffixSheet)
+    print("RefsDesTmp", refsTmp)
+
+for idx, Sheet in enumerate(Sheets):
+    sheet = IPowerLogicSheet(Sheet)
+    #print("Sheet%d" % (1 + idx), "ComponentsCount", sheet.Components.Count)
+    refsNew = []
+    #refSuffixSheet = {}
+    for Comp in sheet.Components:
+        comp = IPowerLogicComp(Comp)
+        comp.Name = comp.Name[1:]
+        refsNew.append(comp.Name)
+
+    print("RefsDesNew", refsNew)
+
+    pass
