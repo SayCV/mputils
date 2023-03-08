1764e5154bf68d953d21b720d554b47ab04fa41f
SayCV <sssxyc@163.com>
2023-03-08 09:11:32 +0800

    add setuptools support

---
+++ setup.py
@@ -0,0 +1,35 @@
+import sys
+
+from setuptools import setup
+
+if sys.version_info < (3, 0):
+    sys.exit("Sorry, Python < 3.0 is not supported")
+
+import re
+
+VERSIONFILE = "mputils/_version.py"
+verstrline = open(VERSIONFILE, "rt").read()
+VSRE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+mo = re.search(VSRE, verstrline, re.M)
+if mo:
+    verstr = mo.group(1)
+else:
+    raise RuntimeError("Unable to find version string in %s." % (VERSIONFILE,))
+
+install_requires = [
+    "pywin32 >= 227",
+]
+
+setup(
+    name="mputils",
+    version=verstr,
+    description="mputils",
+    long_description="Please visit `Github <https://github.com/saycv/mputils>`_ for more information.",
+    author="mputils project developers",
+    author_email="",
+    url="https://github.com/saycv/mputils",
+    packages=["mputils"],
+    entry_points={},
+    include_package_data=True,
+    install_requires=install_requires,
+)
